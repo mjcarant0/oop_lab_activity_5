11e38c78dbebcd6d7c5b0b803408a22ebfbdb1f2
mjcarant0 <marjoycaranto@gmail.com>
2025-05-06 17:18:29 +0800

    Call methods on Lion, Bird, and Reptile instances

--- test_zoo.py
+++ test_zoo.py
@@ -6,3 +6,18 @@ from zoo_reptile import Reptile
 lion = Lion("Shera", 21)
 bird = Bird("Chile", 30)
 reptile = Reptile("Seraph", 12)
+
+# lion
+lion.describe()
+lion.make_sound()
+lion.roar()
+
+# bird
+bird.describe()
+bird.make_sound()
+bird.fly()
+
+# reptile
+reptile.describe()
+reptile.make_sound()
+reptile.crawl()
