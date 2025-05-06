074c8d41247e014740b50214f92f447b09d6ac8b
mjcarant0 <marjoycaranto@gmail.com>
2025-05-06 16:40:10 +0800

    Add Reptile subclass with custom sound and crawl method

---
+++ zoo_reptile.py
@@ -0,0 +1,8 @@
+from zoo_animals import Animal
+
+class Reptile(Animal): 
+    def make_sound(self): # lion sound
+        print("Hiss!")
+    
+    def crawl(self): # reptile crawl
+        print("Crawls stealthily.")
