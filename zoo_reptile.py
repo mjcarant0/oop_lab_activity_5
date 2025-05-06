a9f7094307ec2e035a83f9b593815706aa96ddd7
mjcarant0 <marjoycaranto@gmail.com>
2025-05-06 16:41:08 +0800

    Fix typo in Reptile class comment for sound method

--- zoo_reptile.py
+++ zoo_reptile.py
@@ -1,7 +1,7 @@
 from zoo_animals import Animal
 
 class Reptile(Animal): 
-    def make_sound(self): # lion sound
+    def make_sound(self): # reptile sound
         print("Hiss!")
     
     def crawl(self): # reptile crawl
