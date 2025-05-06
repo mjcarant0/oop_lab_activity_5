b43879b707f7f923f5d65cd39e54aa09c211d4f7
mjcarant0 <marjoycaranto@gmail.com>
2025-05-06 16:35:48 +0800

    Add Bird subclass with custom sound and fly method

---
+++ zoo_bird.py
@@ -0,0 +1,8 @@
+from zoo_animals import Animal
+
+class Bird(Animal): 
+    def make_sound(self): # bird sound
+        print("Chirp!")
+    
+    def fly(self): # bird fly
+        print("Flies high in the sky.")
