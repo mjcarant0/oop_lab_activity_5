d882bb23a3dd4a524bed81d8fd5d24ebc0e46764
mjcarant0 <marjoycaranto@gmail.com>
2025-05-06 16:27:42 +0800

    Add sound and description method

--- zoo_animals.py
+++ zoo_animals.py
@@ -1,4 +1,11 @@
-class Animal:
+class Animal: 
     def __init__(self, name, age):
+        # instance variables
         self.name = name
         self.age = age
+    
+    def make_sound(self): # generic animal sound
+        print("Makes a generic animal sound.")
+
+    def describe(self): # describe the animal
+        print(f"This is {self.name} ({self.age} years old)")
