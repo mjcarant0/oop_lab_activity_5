27e159fdbde31d95e52785764aff81cf3c9c3131
mjcarant0 <marjoycaranto@gmail.com>
2025-05-06 16:38:05 +0800

    Add Lion subclass with custom sound and roar method

---
+++ zoo_lion.py
@@ -0,0 +1,8 @@
+from zoo_animals import Animal
+
+class Lion(Animal): 
+    def make_sound(self): # lion sound
+        print("Roar!")
+    
+    def roar(self): # lion roar
+        print("Roars loudly.")
